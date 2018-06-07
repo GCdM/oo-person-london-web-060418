46127a49a596624f27175d83ff25a302982a4b3d
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-06-07 10:31:21 +0000

    swapping from Learn IDE

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,32 @@
 # your code goes here
+require 'pry'
+
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def happiness=(num)
+    @happiness = num
+    if @happiness > 10
+      @happiness = 10
+    elsif @happiness < 0
+      @happiness = 0
+    end
+  end
+  
+  def hygiene=(num)
+    @hygiene = num
+    if @hygiene > 10
+      @hygiene = 10
+    elsif @hygiene < 0
+      @hygiene = 0
+    end
+  end
+end
