dbac328370ede0878bec5d0ef598e999f8d8089e
GCdM <gabrielcm.br@gmail.com>
2018-06-07 12:51:14 +0100

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -4,14 +4,14 @@ require 'pry'
 class Person
   attr_reader :name, :happiness, :hygiene
   attr_accessor :bank_account
-  
+
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
   end
-  
+
   def happiness=(num)
     @happiness = num
     if @happiness > 10
@@ -20,7 +20,11 @@ class Person
       @happiness = 0
     end
   end
-  
+
+  def happy?
+    @happiness > 7 ? true : false
+  end
+
   def hygiene=(num)
     @hygiene = num
     if @hygiene > 10
@@ -28,5 +32,35 @@ class Person
     elsif @hygiene < 0
       @hygiene = 0
     end
+  end
+
+  def clean?
+    @hygiene > 7 ? true : false
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(person)
+    self.happiness += 3
+    person.happiness += 3
+    "Hi #{person.name}! It's #{@name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+
   end
 end
